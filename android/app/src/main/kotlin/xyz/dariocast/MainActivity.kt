f0ace4f7506957cf747e7250389158462f9d11a8
Dario Castellano <dario.castellano@etuitus.it>
2023-02-03 16:24:22 +0100

    very_ggod template initial

---
+++ android/app/src/main/kotlin/xyz/dariocast/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.dariocast.pomodart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:pomodart/app/app.dart';
+import 'package:pomodart/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
